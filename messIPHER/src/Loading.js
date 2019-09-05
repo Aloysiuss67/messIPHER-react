2480abe8b8e00a7ef40aa461ac04ae2f6ea2bfd4
Benjamin Curran <curranbenj1@ecs.vuw.ac.nz>
2019-09-06 11:54:22 +1200

    Firebase integration seemingly working

--- messIPHER/src/Loading.js
+++ messIPHER/src/Loading.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
-// import firebase from 'react-native-firebase'
+import firebase from 'react-native-firebase'
 
 // export default class Loading extends React.Component {
 //     componentDidMount() {
@@ -20,11 +20,12 @@ export default class Loading extends React.Component {
         )
     }
 
-    // componentDidMount() {
-    //     firebase.auth().onAuthStateChanged(user => {
-    //         this.props.navigation.navigate(user ? 'Home' : 'Register');
-    //     });
-    // }
+    componentDidMount() {
+        //this.props.navigation.navigate(false ? 'Home' : 'Register')
+        firebase.auth().onAuthStateChanged(user => {
+            this.props.navigation.navigate(user ? 'Home' : 'Register');
+        });
+    }
 }
 
 const styles = StyleSheet.create({
